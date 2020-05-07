8f0082a0f3672cc0ac5d58ee38d9204bdff3b522
Evan Ray <eray@mtholyoke.edu>
2020-05-07 12:23:13 -0400

    simulation study estimation scripts

---
+++ scripts/run_simstudy_sir_estimation.py
@@ -0,0 +1,32 @@
+import sys
+import argparse
+import covid.util as util
+import configs
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Run Bayesian compartmental models.')
+    parser.add_argument('place', help='place to use (e.g., US state)')
+    parser.add_argument('--start', help='start date', default='2020-03-04')
+    parser.add_argument('--end', help='end date', default=None)
+    parser.add_argument('--prefix', help='path prefix for saving results', default='results')
+    parser.add_argument('--config', help='model configuration name', default='SEIRD')
+    
+    args = parser.parse_args()
+
+    if args.config not in dir(configs):
+        print(f'Invalid config: {args.config}. Options are {dir(configs)}')
+        exit()
+
+    config = getattr(configs, args.config)
+        
+    data = util.load_data()
+    
+    util.run_place(data,
+                   args.place,
+                   start=args.start,
+                   end=args.end,
+                   prefix=args.prefix,
+                   model_type=config['model'],
+                   T_future=0,
+                   **config['args'])
